09d92e6d212ca7c9177f44cd1d1350b3a0f3b218
Brandon Pedersen <bpedersen@getjive.com>
2014-08-13 23:08:35 -0600

    [US5129] Make rules engine lifecycled and remove update method
    Updates will be performed by the rules engine itself

--- rules/src/main/java/com/jive/myco/jazz/api/rules/czar/model/RulesEngine.java
+++ rules/src/main/java/com/jive/myco/jazz/api/rules/czar/model/RulesEngine.java
@@ -1,23 +1,16 @@
 package com.jive.myco.jazz.api.rules.czar.model;
 
+import com.jive.myco.commons.lifecycle.ListenableLifecycled;
 
 /**
  * Engine to evaluate and apply rule sets.
  *
  * @author jnorton
  */
-public interface RulesEngine
+public interface RulesEngine extends ListenableLifecycled
 {
   /**
    * Apply the current set of rules to the Jazz Context.
    */
   void applyRules();
-
-  /**
-   * Update the current set of rules used in {@link #applyRules()}.
-   *
-   * @param ruleSet
-   *          the new rules to use
-   */
-  void updateRules(final RuleSet ruleSet);
 }
