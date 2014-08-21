f048a48825144b5de519046a5d5f33e711e80b7c
David Valeri <dvaleri@getjive.com>
2014-08-21 11:09:12 -0600

    [US5273] Deprecated non-async reg binding remove.

--- registry/src/main/java/com/jive/myco/jazz/api/registry/RegisteredServiceInstanceBinding.java
+++ registry/src/main/java/com/jive/myco/jazz/api/registry/RegisteredServiceInstanceBinding.java
@@ -2,6 +2,8 @@ package com.jive.myco.jazz.api.registry;
 
 import java.util.Map;
 
+import com.jive.myco.commons.concurrent.PnkyPromise;
+
 /**
  * Represents information about a registered service instance and the actions that may be applied to
  * the instance.
@@ -12,8 +14,16 @@ public interface RegisteredServiceInstanceBinding
 {
   /**
    * Unregisters the service instance from the registry.
+   *
+   * @deprecated signature is changing, use removeAsync in the interim
    */
+  @Deprecated
   void remove();
+
+  /**
+   * Unregisters the service instance from the registry.
+   */
+  PnkyPromise<Void> removeAsync();
 
   /**
    * Updates the service instance data in the registry.
