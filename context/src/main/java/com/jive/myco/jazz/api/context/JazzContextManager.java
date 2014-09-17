a67f3a5eb7fdfe202a44d972a4e2c73bc858ff11
David Valeri <dvaleri@getjive.com>
2014-09-17 10:27:31 -0600

    [US5529] Added private scope to JazzContextManager to avoid needing to filter properties on outbound hops.

--- context/src/main/java/com/jive/myco/jazz/api/context/JazzContextManager.java
+++ context/src/main/java/com/jive/myco/jazz/api/context/JazzContextManager.java
@@ -1,20 +1,23 @@
 package com.jive.myco.jazz.api.context;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import org.apache.commons.lang.RandomStringUtils;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 
 /**
@@ -58,30 +61,36 @@ public final class JazzContextManager
   /**
    * Context that is local to the system and global across the application
    */
-  private static volatile Map<String, String> localContext = Maps.newConcurrentMap();
+  private static volatile Map<String, ContextProperty> immutableLocalContext = Maps
+      .newConcurrentMap();
 
   /**
    * Context that is available on a per-thread/context basis
    */
-  private static final ThreadLocal<Map<String, String>> context = ThreadLocal
+  private static final ThreadLocal<Map<String, ContextProperty>> context = ThreadLocal
       .withInitial(JazzContextManager::createContext);
 
   private static final JazzContextManager INSTANCE = new JazzContextManager();
 
   /**
-   * Initialize the local context with the given properties.
+   * Initialize the local context with the given immutable private properties.
    *
-   * @param localContext
-   *          the local context values to use
+   * @param immutablePrivateProperties
+   *          the immutable private context values to use
    */
-  public static void initialize(final Map<String, String> localContext)
+  public static void initialize(final Map<String, String> immutablePrivateProperties)
   {
-    JazzContextManager.localContext = ImmutableMap.copyOf(localContext);
+    JazzContextManager.immutableLocalContext = Collections.unmodifiableMap(
+        immutablePrivateProperties.entrySet()
+            .stream()
+            .collect(
+                Collectors.<Map.Entry<String, String>, String, ContextProperty> toMap(
+                    Map.Entry::getKey,
+                    (entry) -> new ContextProperty(entry.getValue(), Scope.PRIVATE))));
   }
 
   /**
-   * Get a context parameter from the local context first, then from the incoming context if not
-   * available.
+   * Returns a context property, either public or private.
    *
    * @param key
    *          the non-{@code null} context key
@@ -89,82 +98,136 @@ public final class JazzContextManager
    */
   public static String get(final String key)
   {
-    return Optional
-        .ofNullable(localContext.get(key))
-        .orElseGet(() -> context.get().get(key));
+    return get(key, () -> null, null);
   }
 
   /**
-   * Get a context parameter or {@code defaultValue} if not present.
+   * Returns a context property, either public or private, or {@code defaultValue} if not present.
    *
    * @param key
    *          the the non-{@code null} context key
    * @param defaultValue
-   *          the default value if not present
+   *          the default value if {@code key} is not present
+   *
    * @return the context value or default value if not present
    */
   public static String get(final String key, final String defaultValue)
   {
-    return Optional
-        .ofNullable(localContext.get(key))
-        .orElse(context.get().getOrDefault(key, defaultValue));
+    return get(key, () -> defaultValue, null);
   }
 
   /**
-   * Get a context parameter, invoking {@code defaultProvider} if not present for the default value.
+   * Returns a context parameter, either public or private, invoking {@code defaultProvider} if not
+   * present for the default value.
    *
    * @param key
    *          the non-{@code null} context key
    * @param defaultProvider
-   *          provider to invoke if the {@code key} is not present
+   *          provider to invoke if {@code key} is not present
+   *
    * @return the context value or default value if not present
    */
   public static String get(final String key, final Supplier<String> defaultProvider)
   {
-    return Optional
-        .ofNullable(localContext.get(key))
-        .orElse(
-            Optional
-                .ofNullable(context.get().get(key))
-                .orElseGet(defaultProvider));
+    return get(key, defaultProvider, null);
   }
 
   /**
-   * Set a context parameter.
+   * Returns a context parameter, in the requested {@code scope}, invoking {@code defaultProvider}
+   * if not present for the default value.
+   *
+   * @param key
+   *          the non-{@code null} context key
+   * @param defaultProvider
+   *          provider to invoke if {@code key} is not present
+   * @param scope
+   *          the scope to restrict the lookup to or {@code null} for either scope
+   *
+   * @return the context value or default value if not present in the requested scope
+   */
+  public static String get(final String key, final Supplier<String> defaultProvider,
+      final Scope scope)
+  {
+    return Optional
+        .ofNullable(
+            Optional
+                .ofNullable(immutableLocalContext.get(key))
+                .orElseGet(() -> context.get().get(key)))
+        .filter((cp) -> scope == null || cp.getScope() == scope)
+        .map(ContextProperty::getValue)
+        .orElseGet(defaultProvider);
+  }
+
+  /**
+   * Sets a public context property.
    *
    * @param key
    *          the non-{@code null} context key
    * @param value
    *          the context value
+   *
    * @return this instance for chaining
    */
   public static JazzContextManager put(final String key, final String value)
   {
+    return put(key, value, Scope.PUBLIC);
+  }
+
+  /**
+   * Sets a context property in the requested scope.
+   *
+   * @param key
+   *          the non-{@code null} context key
+   * @param value
+   *          the context value
+   * @param scope
+   *          the scope in which to set the property
+   *
+   * @return this instance for chaining
+   */
+  public static JazzContextManager put(final String key, final String value,
+      @NonNull final Scope scope)
+  {
     checkKey(key);
 
-    context.get().put(key, value);
+    context.get().put(key, new ContextProperty(value, scope));
+
     return INSTANCE;
   }
 
   /**
-   * Set the value for {@code key} if the value is absent.
+   * Sets a public context property for {@code key} if the property is absent.
    *
    * @param key
    *          the the non-{@code null} context key
    * @param valueIfAbsent
    *          the value to set if not already set
+   *
    * @return this instance for chaining
    */
   public static JazzContextManager putIfAbsent(final String key, final String valueIfAbsent)
   {
-    checkKey(key);
-
-    context.get().putIfAbsent(key, valueIfAbsent);
-    return INSTANCE;
+    return putIfAbsent(key, valueIfAbsent, Scope.PUBLIC);
   }
 
   /**
-   * Set the value for {@code key} if the value is absent invoking the provided
+   * Sets a context property in the requested scope for {@code key} if the property is absent.
+   *
+   * @param key
+   *          the the non-{@code null} context key
+   * @param valueIfAbsent
+   *          the value to set if not already set
+   *
+   * @return this instance for chaining
+   */
+  public static JazzContextManager putIfAbsent(final String key, final String valueIfAbsent,
+      final Scope scope)
+  {
+    return putIfAbsent(key, () -> valueIfAbsent, scope);
+  }
+
+  /**
+   * Set a public context property for {@code key} if the property is absent, invoking the provided
    * {@code valueSupplier} to get the value to set.
    *
    * @param key
@@ -176,44 +239,120 @@ public final class JazzContextManager
   public static JazzContextManager putIfAbsent(final String key,
       final Supplier<String> valueSupplier)
   {
+    return putIfAbsent(key, valueSupplier, Scope.PUBLIC);
+  }
+
+  /**
+   * Set a context property in the requested scope for {@code key} if the property is absent,
+   * invoking the provided {@code valueSupplier} to get the value to set.
+   *
+   * @param key
+   *          the the non-{@code null} context key
+   * @param valueSupplier
+   *          a supplier to lazily provide the value if absent
+   * @param scope
+   *          the scope in which to set the property
+   *
+   * @return this instance for chaining
+   */
+  public static JazzContextManager putIfAbsent(final String key,
+      final Supplier<String> valueSupplier, final Scope scope)
+  {
     checkKey(key);
 
-    context.get().computeIfAbsent(key, (k) -> valueSupplier.get());
+    final ContextProperty currentProp = context.get().get(key);
+
+    if (currentProp == null || currentProp.getValue() == null)
+    {
+      context.get().put(key, new ContextProperty(valueSupplier.get(), scope));
+    }
+
     return INSTANCE;
   }
 
   /**
-   * Put all the given values into the context.
+   * Put all the given properties into the context as public properties.
    *
    * @param values
-   *          the values to add to the context
+   *          the properties to add to the context
    */
   public static JazzContextManager putAll(@NonNull final Map<String, String> values)
   {
+    return putAll(values, Scope.PUBLIC);
+  }
+
+  /**
+   * Put all the given properties into the context in the requested scope.
+   *
+   * @param values
+   *          the properties to add to the context
+   * @param scope
+   *          the scope in which to set the properties
+   *
+   * @return this instance for chaining
+   */
+  public static JazzContextManager putAll(@NonNull final Map<String, String> values,
+      final Scope scope)
+  {
     values.keySet().forEach(JazzContextManager::checkKey);
 
-    context.get().putAll(values);
+    final Map<String, ContextProperty> props = context.get();
+
+    values.entrySet()
+        .forEach(
+            (entry) -> props.put(entry.getKey(),
+                new ContextProperty(entry.getValue(), scope)));
+
     return INSTANCE;
   }
 
   /**
-   * Put all the given values into the context but only if they do not already exist in the context.
+   * Put all the given properties into the context as public properties but only if they do not
+   * already exist in the context.
    *
    * @param values
    *          the values to add to the context
+   *
    * @return this instance for chaining
    */
   public static JazzContextManager putAllIfAbsent(@NonNull final Map<String, String> values)
   {
+    return putAllIfAbsent(values, Scope.PUBLIC);
+  }
+
+  /**
+   * Put all the given properties into the context with the requested scope but only if they do not
+   * already exist in the context.
+   *
+   * @param values
+   *          the values to add to the context
+   * @param scope
+   *          the scope in which to set the properties
+   *
+   * @return this instance for chaining
+   */
+  public static JazzContextManager putAllIfAbsent(@NonNull final Map<String, String> values,
+      final Scope scope)
+  {
     values.keySet().forEach(JazzContextManager::checkKey);
 
-    values.entrySet().forEach(
-        (entry) -> context.get().putIfAbsent(entry.getKey(), entry.getValue()));
+    final Map<String, ContextProperty> props = context.get();
+
+    values.entrySet()
+        .forEach((entry) ->
+        {
+          final ContextProperty currentProp = props.get(entry.getKey());
+
+          if (currentProp == null || currentProp.getValue() == null)
+          {
+            props.put(entry.getKey(), new ContextProperty(entry.getValue(), scope));
+          }
+        });
     return INSTANCE;
   }
 
   /**
-   * Set the value for {@code key} if the value is absent.
+   * Removes the value for {@code key} if the value is absent.
    *
    * @param key
    *          the key for which to remove the value
@@ -238,13 +377,23 @@ public final class JazzContextManager
   }
 
   /**
-   * Transform the current context into a map which <em>excludes</em> values from the local system
-   * context. The resulting map will not be backed by this instance. Modifications to the returned
-   * map will not affect this context.
+   * Transform the current context into a mutable map which <em>excludes</em> values from the
+   * private scope. The resulting map will not be backed by this instance. Modifications to the
+   * returned map will not affect this context.
    */
   public static Map<String, String> toMap()
   {
-    return new HashMap<>(context.get());
+    return context.get().entrySet()
+        .stream()
+        .filter((entry) -> entry.getValue().getScope() == Scope.PUBLIC)
+        .collect(
+            Collectors.toMap(
+                Map.Entry::getKey,
+                (entry) -> entry.getValue().getValue(),
+                // @formatter:off
+                (u, v) -> { throw new IllegalStateException(String.format("Duplicate key %s", u)); },
+                // @formatter:on
+                HashMap::new));
   }
 
   /**
@@ -255,12 +404,14 @@ public final class JazzContextManager
     return toMap().entrySet().stream();
   }
 
-  private static HashMap<String, String> createContext()
+  private static HashMap<String, ContextProperty> createContext()
   {
-    final HashMap<String, String> newContext = new HashMap<>();
+    final HashMap<String, ContextProperty> newContext = new HashMap<>();
 
     newContext.put(JAZZ_CONTEXT_ID_KEY,
-        RandomStringUtils.randomAlphanumeric(JAZZ_CONTEXT_ID_LENGTH));
+        new ContextProperty(
+            RandomStringUtils.randomAlphanumeric(JAZZ_CONTEXT_ID_LENGTH),
+            Scope.PRIVATE));
 
     return newContext;
   }
@@ -269,5 +420,19 @@ public final class JazzContextManager
   {
     Preconditions.checkArgument(JazzContextManager.KEY_PATTERN.matcher(key).matches(),
         "Invalid context key [%s], only numbers, letters, and . _ - characters allowed", key);
+  }
+
+  @RequiredArgsConstructor
+  @Getter
+  private static final class ContextProperty
+  {
+    private final String value;
+    private final Scope scope;
+  }
+
+  public static enum Scope
+  {
+    PRIVATE,
+    PUBLIC;
   }
 }
