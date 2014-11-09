26117251d721c9564e7cdf74a3465810e96ff75f
David Valeri <dvaleri@getjive.com>
2014-11-09 12:15:26 -0700

    [NS] Deprecated raw servlet binding in HttpServerManager.  Added an ID to web app descriptor.

--- web/src/main/java/com/jive/myco/jazz/api/web/WebAppDescriptor.java
+++ web/src/main/java/com/jive/myco/jazz/api/web/WebAppDescriptor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import lombok.Getter;
 import lombok.NonNull;
@@ -17,6 +18,10 @@ import lombok.experimental.Builder;
 @Getter
 public final class WebAppDescriptor
 {
+  private final AtomicInteger INSTANCE_COUNTER = new AtomicInteger();
+
+  private final String id;
+
   private final List<ServletMappingDescriptor> servlets;
 
   private final List<FilterMappingDescriptor> filters;
@@ -31,6 +36,7 @@ public final class WebAppDescriptor
 
   @Builder
   private WebAppDescriptor(
+      final String id,
       @NonNull final List<ServletMappingDescriptor> servlets,
       @NonNull final List<FilterMappingDescriptor> filters,
       @NonNull final List<StaticResourceDescriptor> staticResources,
@@ -38,6 +44,7 @@ public final class WebAppDescriptor
       final boolean includeJazzMdcFilter,
       final boolean includeJazzContextEnhancerRulesFilter)
   {
+    this.id = id == null ? "web-app-" + INSTANCE_COUNTER.getAndIncrement() : id;
     this.servlets =
         Collections.unmodifiableList(new ArrayList<>(servlets));
     this.filters =
