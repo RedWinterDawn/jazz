c998550698c02e27319114f6a6b09d5be820141c
Binh Tran <btran@jive.com>
2014-12-22 13:13:09 -0700

    Add filterable url to RestClientBuilder

--- rest-client/src/main/java/com/jive/myco/jazz/api/rest/client/RestClientBuilder.java
+++ rest-client/src/main/java/com/jive/myco/jazz/api/rest/client/RestClientBuilder.java
@@ -1,16 +1,21 @@
 package com.jive.myco.jazz.api.rest.client;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Executor;
 import java.util.function.Supplier;
 
 import javax.ws.rs.ext.ParamConverterProvider;
 
+import com.google.common.base.Function;
+import com.jive.myco.jazz.api.rest.client.ExceptionHandler.RestExceptionHandlerProvider;
 import com.jive.myco.jazz.api.web.HttpClientHeaderDecorator;
 
 /**
  * Builder for a REST client.
  *
  * @author David Valeri
+ * @author Binh Tran
  *
  * @param <T>
  *          the interface type for the returned client
@@ -25,9 +30,13 @@ public interface RestClientBuilder<T>
 
   RestClientBuilder<T> addProvider(final ParamConverterProvider provider);
 
+  RestClientBuilder<T> addProvider(final RestExceptionHandlerProvider restExceptionHandlerProvider);
+
   RestClientBuilder<T> url(final String url);
 
   RestClientBuilder<T> url(final Supplier<String> urlSupplier);
+
+  RestClientBuilder<T> url(final Function<List<String>, Optional<String>> filterFunction);
 
   RestClientBuilder<T> headerDecorator(final HttpClientHeaderDecorator headerDecorator);
 
