4f7a97e2c665927cce46d7f6993075f6af0df5e0
David Valeri <dvaleri@getjive.com>
2015-01-28 22:24:41 -0700

    [US7821] Updated health checks to pass ID to AbstractLifecycled.

--- health/src/main/java/com/jive/myco/jazz/api/health/AbstractPeriodicSlidingWindowHistogramHealthCheck.java
+++ health/src/main/java/com/jive/myco/jazz/api/health/AbstractPeriodicSlidingWindowHistogramHealthCheck.java
@@ -71,7 +71,7 @@ public abstract class AbstractPeriodicSlidingWindowHistogramHealthCheck extends
    * @param value
    *          the value to add to record in the reservoir
    */
-  public void update(final long value)
+  public final void update(final long value)
   {
     // This is a bit hacky, but we are not concerned with absolute ordering or ensuring that we
     // catch every event during the transition from grace period to normal operation. As such,
