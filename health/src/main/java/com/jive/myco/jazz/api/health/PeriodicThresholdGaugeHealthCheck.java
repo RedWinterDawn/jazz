4749541044d5c0d43021f367ce945d7d244337be
David Valeri <dvaleri@getjive.com>
2015-06-01 08:00:27 -0600

    Revert "Merge workspace [us9621-reporting-hikari-metrics]"
    This reverts commit 79f7404ad9439e8f0517f38df9e2eddcdc0bd816, reversing
    changes made to df25af7fca91bfdc94ea1f334d743969c9bbefc5.

--- health/src/main/java/com/jive/myco/jazz/api/health/PeriodicThresholdGaugeHealthCheck.java
+++ health/src/main/java/com/jive/myco/jazz/api/health/PeriodicThresholdGaugeHealthCheck.java
@@ -4,10 +4,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.BiFunction;
 import java.util.function.Supplier;
 
-import lombok.Builder;
 import lombok.NonNull;
-import lombok.Setter;
-import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 
 import com.jive.myco.commons.concurrent.Pnky;
@@ -123,14 +120,11 @@ public final class PeriodicThresholdGaugeHealthCheck<T extends Comparable<T>> ex
     return new PeriodicThresholdGaugeHealthCheckBuilder<>();
   }
 
-  @Accessors(fluent = true)
-  @Setter
   public static final class PeriodicThresholdGaugeHealthCheckBuilder<T extends Comparable<T>>
       extends AbstractAsyncPeriodicHealthCheckBuilder<PeriodicThresholdGaugeHealthCheckBuilder<T>>
   {
-    private final ThresholdMode thresholdMode = ThresholdMode.UPPER_BOUND;
-    private String id;
     private Supplier<PnkyPromise<T>> dataSupplier;
+    private final ThresholdMode thresholdMode = ThresholdMode.UPPER_BOUND;
     private T infoThreshold;
     private T warnThreshold;
     private T criticalThreshold;
@@ -151,6 +145,32 @@ public final class PeriodicThresholdGaugeHealthCheck<T extends Comparable<T>> ex
           infoThreshold,
           warnThreshold,
           criticalThreshold);
+
+    }
+
+    public PeriodicThresholdGaugeHealthCheckBuilder<T> dataSupplier(
+        final Supplier<PnkyPromise<T>> dataSupplier)
+    {
+      this.dataSupplier = dataSupplier;
+      return this;
+    }
+
+    public PeriodicThresholdGaugeHealthCheckBuilder<T> infoThreshold(final T infoThreshold)
+    {
+      this.infoThreshold = infoThreshold;
+      return this;
+    }
+
+    public PeriodicThresholdGaugeHealthCheckBuilder<T> warnThreshold(final T warnThreshold)
+    {
+      this.warnThreshold = warnThreshold;
+      return this;
+    }
+
+    public PeriodicThresholdGaugeHealthCheckBuilder<T> criticalThreshold(final T criticalThreshold)
+    {
+      this.criticalThreshold = criticalThreshold;
+      return this;
     }
 
     @Override
